5d0f9d49fca9d412c361b2ab45640d4afa802318
Manuel Cabrera <mjcd.github@gmail.com>
2024-07-15 00:21:28 -0400

    ajustes en seccion quienes somos, tooltips, navbar responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,29 @@
-$(document).ready(function(){
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
-  
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-  
-        // Store hash
-        var hash = this.hash;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 100, function(){
-  
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      } // End if
+$(document).ready(
+  function () {
+
+    // Tooltips
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+    // Controla los CARD que estan a los costados en la seccion "Quienes Somos" oculta el texto con desplazamiento suave
+    $(".card-header").click(
+      function () {
+        $(this).next(".card-body").find(".card-text").slideToggle();
+      }
+    );
+
+    // Controla el CARD que tiene el header abajo en la seccion "Quienes Somos" oculta el texto con desplazamiento suave
+    $(document).ready(function () {
+      $(".card-header").click(function () {
+        $(this).prev(".card-body").find(".card-text").slideToggle();
+      });
     });
-  });
+
+    // Carrusel con desplazamiento automatico
+    var myCarousel = document.querySelector('#carouselExampleIndicators')
+    var carousel = new bootstrap.Carousel(myCarousel, {
+      interval: 3000, // Intervalo de tiempo en milisegundos entre cada slide
+      ride: 'carousel' // Inicializa automáticamente el carrusel
+    })
+  }
+)
